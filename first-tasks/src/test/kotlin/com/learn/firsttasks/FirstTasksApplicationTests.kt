2e2454addf3cff53a9c91aacb77f1966ddfa8ec4
svkaizoku <svignesh81235@gmail.com>
2023-07-24 23:43:57 +0200

    Adding to branch 2

---
+++ first-tasks/src/test/kotlin/com/learn/firsttasks/FirstTasksApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.learn.firsttasks
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FirstTasksApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
